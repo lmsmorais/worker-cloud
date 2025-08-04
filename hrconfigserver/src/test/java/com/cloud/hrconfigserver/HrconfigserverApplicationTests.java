f9504a425e9195781d54c8e660b4df310b592c3d
lmsmorais <lms.morais@outlook.com>
2025-08-04 19:32:43 -0300

    create project hrconfigserver

---
+++ hrconfigserver/src/test/java/com/cloud/hrconfigserver/HrconfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.hrconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrconfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
