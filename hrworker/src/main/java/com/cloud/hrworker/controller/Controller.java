ace08bc28624ad9613bff527d5e4826269cf5892
lmsmorais <lms.morais@outlook.com>
2025-08-06 19:07:19 -0300

    config server client in hrworker project

--- hrworker/src/main/java/com/cloud/hrworker/controller/Controller.java
+++ hrworker/src/main/java/com/cloud/hrworker/controller/Controller.java
@@ -2,7 +2,10 @@ package com.cloud.hrworker.controller;
 
 import com.cloud.hrworker.entities.Worker;
 import com.cloud.hrworker.repository.WorkerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,8 +18,19 @@ import java.util.List;
 @RequestMapping("/workers")
 public class Controller {
 
+    private static final Logger logger = LoggerFactory.getLogger(Controller.class);
+
+    @Value("${test.config}")
+    private String testConfig;
+
     @Autowired
     private WorkerRepository repository;
+
+    @GetMapping(value = "/configs")
+    public ResponseEntity<Void> getConfigs(){
+        logger.info("CONFIG= {}", testConfig);
+        return ResponseEntity.noContent().build();
+    }
 
     @GetMapping
     public ResponseEntity<List<Worker>> findAll(){
