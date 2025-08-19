a04a0c9b79b8e52c1ac8c053a42d3857697bc8f3
lmsmorais <lms.morais@outlook.com>
2025-08-19 19:18:10 -0300

    adding project hruser

---
+++ hruser/src/main/java/com/cloud/hruser/HruserApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.hruser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HruserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HruserApplication.class, args);
+	}
+
+}
