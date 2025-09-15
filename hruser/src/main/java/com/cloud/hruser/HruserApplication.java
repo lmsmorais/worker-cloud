d8a55de140494df0e85e86b042a215f29dc62802
lmsmorais <lms.morais@outlook.com>
2025-09-15 19:24:25 -0300

    configuring BCryptPasswordEncoder database seeding

--- hruser/src/main/java/com/cloud/hruser/HruserApplication.java
+++ hruser/src/main/java/com/cloud/hruser/HruserApplication.java
@@ -1,13 +1,23 @@
 package com.cloud.hruser;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class HruserApplication {
+public class HruserApplication implements CommandLineRunner {
+
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(HruserApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		//System.out.println("BYCRYPT = " + passwordEncoder.encode("testandosenha"));
+	}
 }
