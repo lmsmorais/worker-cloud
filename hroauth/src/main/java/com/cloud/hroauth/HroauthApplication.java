466c35cb6a0676dae2e68dd628895777dd7d45db
lmsmorais <lms.morais@outlook.com>
2025-09-16 19:24:13 -0300

    create project hrouath

---
+++ hroauth/src/main/java/com/cloud/hroauth/HroauthApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.hroauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HroauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HroauthApplication.class, args);
+	}
+
+}
